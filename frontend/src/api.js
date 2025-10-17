3cfaa035f09c633da2a654ec36b48fa3b95b036b
Rochakneup <114130210+Rochakneup@users.noreply.github.com>
2025-10-17 13:42:16 +0545

    api changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/siruvai";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 export const IMAGE_URL = import.meta.env.IMAGE_URL || "http://localhost:5000";
